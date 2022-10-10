97733f0811b6d948406ce2d7d64d1ce3f8bd3a20
JagdpantherK <jagpanthertitanium@gmail.com>
2022-10-11 04:31:22 +0600

    Initial commit

---
+++ src/main/java/com/example/bank_of_ruslan/BankOfRuslanApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_of_ruslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfRuslanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankOfRuslanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bank_of_ruslan/BankOfRuslanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_of_ruslan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOfRuslanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
